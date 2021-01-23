8c469419e53dacf17470e4eb3cc81e2a1a57e29a
周樹禾 <shuho.chou@gmail.com>
2021-01-23 10:38:12 +0800

    added minified js

--- rollup.sh
+++ rollup.sh
@@ -1,6 +1,6 @@
 # https://rollupjs.org/guide/en/
-rollup runtime.js --name runtime --format iife --silent | terser -cm -o docs/runtime.min.js
-rollup main.js --name main --format iife --silent | terser -cm -o docs/main.min.js
-cp -Rf images/ docs/images
-cp -Rf style/ docs/style
-cp -f manifest.webmanifest docs/
+rollup runtime.js --name runtime --format iife --silent | terser -cm -o runtime.min.js
+rollup main.js --name main --format iife --silent | terser -cm -o main.min.js
+# cp -Rf images/ docs/images
+# cp -Rf style/ docs/style
+# cp -f manifest.webmanifest docs/
