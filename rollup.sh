6994eb5bc0cac8133efbd85a5bf7451be5b303f5
周樹禾 <shuho.chou@gmail.com>
2021-01-23 09:24:35 +0800

    copy manifest.webmanifest to docs

--- rollup.sh
+++ rollup.sh
@@ -3,3 +3,4 @@ rollup runtime.js --name runtime --format iife --silent | terser -cm -o docs/run
 rollup main.js --name main --format iife --silent | terser -cm -o docs/main.min.js
 cp -Rf images/ docs/images
 cp -Rf style/ docs/style
+cp -f manifest.webmanifest docs/
