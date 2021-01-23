9ae716983958022d9ebf091a94e50b2410d1391d
周樹禾 <shuho.chou@gmail.com>
2021-01-23 10:49:45 +0800

    remove public, not required anymore

--- rollup.sh
+++ rollup.sh
@@ -1,6 +1,3 @@
 # https://rollupjs.org/guide/en/
 rollup runtime.js --name runtime --format iife --silent | terser -cm -o runtime.min.js
 rollup main.js --name main --format iife --silent | terser -cm -o main.min.js
-# cp -Rf images/ docs/images
-# cp -Rf style/ docs/style
-# cp -f manifest.webmanifest docs/
