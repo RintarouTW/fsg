901e97e11a33b287554fa92c32223edd5461e11f
周樹禾 <shuho.chou@gmail.com>
2021-01-23 09:09:42 +0800

    rename public to docs for github pages

--- rollup.sh
+++ rollup.sh
@@ -1,5 +1,5 @@
 # https://rollupjs.org/guide/en/
-rollup runtime.js --name runtime --format iife --silent | terser -cm -o public/runtime.min.js
-rollup main.js --name main --format iife --silent | terser -cm -o public/main.min.js
-cp -Rf images/ public/images
-cp -Rf style/ public/style
+rollup runtime.js --name runtime --format iife --silent | terser -cm -o docs/runtime.min.js
+rollup main.js --name main --format iife --silent | terser -cm -o docs/main.min.js
+cp -Rf images/ docs/images
+cp -Rf style/ docs/style
