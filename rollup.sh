4b9b3b4e696190d37ab27fc8919fced713ae9377
周樹禾 <shuho.chou@gmail.com>
2021-01-23 09:07:33 +0800

    add public for github pages

--- rollup.sh
+++ rollup.sh
@@ -1,3 +1,5 @@
 # https://rollupjs.org/guide/en/
-rollup runtime.js --name runtime --format iife --silent | terser -cm -o runtime.min.js
-rollup main.js --name main --format iife --silent | terser -cm -o main.min.js
+rollup runtime.js --name runtime --format iife --silent | terser -cm -o public/runtime.min.js
+rollup main.js --name main --format iife --silent | terser -cm -o public/main.min.js
+cp -Rf images/ public/images
+cp -Rf style/ public/style
