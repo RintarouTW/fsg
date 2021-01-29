b1d39bd683d65d8f2d5de598512a1f2356e5d771
周樹禾 <shuho.chou@gmail.com>
2021-01-30 06:11:32 +0800

    refined menu and menu items

--- module/menu.js
+++ module/menu.js
@@ -7,44 +7,88 @@ import { getHash, postCode } from './server.js'
 
 const MENU_PADDING_LEFT = 10 
 const MENU_PADDING_TOP = 5
+const MENU_PADDING_BOTTOM = 5
+const MENU_ITEM_HEIGHT = 23
+const MENU_BACKGROUND_COLOR = '#333'
+const MENU_BORDER_COLOR = '#666'
+const MENU_SEPARATOR_COLOR = '#555'
 
-class Menu {
-  constructor(draw, coord) {
-    this.draw = draw
-    const menu = draw.group().translate(coord.x, coord.y).attr('class', 'menu')
-    menu.rect(100, 50).flip('y')  // background
-      .stroke({ color:'#666', width: 0.3})
-      .fill('#000')
-    const item = menu.text('Edit').flip('y')
+///
+/// MenuItem/
+/// menu : element
+///
+
+class MenuItem {
+  constructor(draw, menu, text) {
+    const item = menu.text(text).flip('y')
       .attr('class', 'menu_item')
-      .move(MENU_PADDING_LEFT, MENU_PADDING_TOP)
     item.on('mousedown', evt => {
-      this.editSVG(draw)
+      this.onMouseDown(draw)
       evt.preventDefault()
       evt.stopPropagation()
     })
+    item.menu = menu
+    this.item = item
+  }
+  move(x, y) {
+    this.item.move(x, y)
+  }
+  onMouseDown(draw) {
+    console.warnning('this should be overriden')
+  }
+}
+
+class Menu {
+  constructor(draw, coord, title) {
+    this.draw = draw
+    const menu = draw.group().translate(coord.x, coord.y).attr('class', 'menu')
+    const bg = menu.rect(100, 50).flip('y')  // background
+      .stroke({ color: MENU_BORDER_COLOR, width: 0.3})
+      .fill(MENU_BACKGROUND_COLOR)
+    menu.text(title).flip('y')
+      .attr('class', 'menu_title')
+      .move(MENU_PADDING_LEFT, MENU_PADDING_TOP)
+    this.numItems = 0
     this.menu = menu
+    this.background = bg
     draw.menu = this
+  }
+  addMenuItem(item) {
+    this.numItems++
+    const y = MENU_PADDING_TOP + MENU_ITEM_HEIGHT * this.numItems
+    this.menu.line(MENU_PADDING_LEFT - 5, y, 105 - MENU_PADDING_LEFT, y).flip('y')
+      .stroke(MENU_SEPARATOR_COLOR)
+    item.move(MENU_PADDING_LEFT, y + 3)
+    // update background
+    this.background.size(100, y + MENU_ITEM_HEIGHT + MENU_PADDING_BOTTOM)
   }
   remove() {
     this.menu.remove()
     this.draw.menu = null
   }
-  editSVG(draw) {
-    this.remove()
-    const code = { code : draw.parent().svg() }
-    getHash().then( json => {
-      const hash = json.hash
-      postCode(hash, code).then( () => {
-        window.open(SERVER_ROOT + '?source=pwa&hash=' + hash,
-          '_blank',
-          `resizable, width=${DEFAULT_WINDOW_WIDTH},height=${DEFAULT_WINDOW_HEIGHT}`)
+}
+
+class RuntimeMenu extends Menu {
+  constructor(draw, coord) {
+    super(draw, coord, 'Menu')
+    const editItem = new MenuItem(draw, this.menu, 'Edit')
+    editItem.onMouseDown = () => {
+      this.remove()
+      const code = { code : draw.parent().svg() }
+      getHash().then( json => {
+        const hash = json.hash
+        postCode(hash, code).then( () => {
+          window.open(SERVER_ROOT + '?source=pwa&hash=' + hash,
+            '_blank',
+            `resizable, width=${DEFAULT_WINDOW_WIDTH},height=${DEFAULT_WINDOW_HEIGHT}`)
+        }).catch( error => console.log(error) )
       }).catch( error => console.log(error) )
-    }).catch( error => console.log(error) )
+    }
+    this.addMenuItem(editItem)
   }
 }
 
 export function gen_menu(draw, coord) {
-  return new Menu(draw, coord)
+  return new RuntimeMenu(draw, coord)
 }
 
