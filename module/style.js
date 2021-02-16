8158a54b2548e42e27b4c96ab16c43fbf1e8292e
周樹禾 <shuho.chou@gmail.com>
2021-02-17 02:57:39 +0800

    removed unused code

--- module/style.js
+++ module/style.js
@@ -1,54 +1,5 @@
 'use strict'
 
-function getStyle(id, key = 'stroke') {
-  const element = SVG(id)
-  console.assert(element)
-  return element.node.computedStyleMap().get(key).toString()
-}
-
-// ex: setStyle('#c1', 'fill', '#ccc')
-function setStyle(id, key, value) {
-  if (typeof value === 'undefined') value = null
-  const element = SVG(id); console.assert(element)
-  element.attr(key, value)
-}
-
-export function getStrokeColor(id) {
-  const element = SVG(id)
-  console.assert(element, `element with ${id} must exist`)
-  return getStyle(id, 'stroke')
-}
-
-export function setStrokeColor(id, color) {
-  const element = SVG(id)
-  console.assert(element, `element with ${id} must exist`)
-  setStyle(id, 'stroke', color)
-}
-
-export function getStrokeWidth(id) {
-  const element = SVG(id)
-  console.assert(element, `element with ${id} must exist`)
-  return getStyle(id, 'stroke-width')
-}
-
-export function setStrokeWidth(id) {
-  const element = SVG(id)
-  console.assert(element, `element with ${id} must exist`)
-  setStyle(id, 'stroke-width', color)
-}
-
-export function getFillColor(id) {
-  const element = SVG(id)
-  console.assert(element, `element with ${id} must exist`)
-  return getStyle(id, 'fill')
-}
-
-export function setFillColor(id, color) {
-  const element = SVG(id)
-  console.assert(element, `element with ${id} must exist`)
-  setStyle(id, 'color', color)
-}
-
 ///
 /// Styles
 ///
