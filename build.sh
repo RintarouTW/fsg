9b7fdcb9f446cb6b3d65c4265706d256b9ff3fb4
周樹禾 <shuho.chou@gmail.com>
2021-03-08 20:37:32 +0800

    strip for runtime.min.js

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 ## npm install -g @rollup/plugin-strip
 # https://terser.org/
 ## npm install -g terser
-rollup runtime.js --name runtime --format iife --silent | terser -cm -o runtime.min.js
-# rollup runtime.js --name runtime --format iife --silent -p strip | terser -cm -o runtime.min.js
+# rollup runtime.js --name runtime --format iife --silent | terser -cm -o runtime.min.js
+rollup runtime.js --name runtime --format iife --silent -p strip | terser -cm -o runtime.min.js
 # rollup main.js --name main --format iife --silent | terser -cm -o main.min.js
 rollup main.js --name main --format iife --silent -p strip | terser -cm -o main.min.js
